24c7f49c4a9dd6ef92d7e88f43e23d1ef06390d4
pedrorfmlopes-sys <pedrorfmlopes@gmail.com>
2026-03-05 22:38:48 +0000

    fix(lint): ignore build artifacts in eslint.config.js

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -3,6 +3,9 @@ import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 
 export default [
+  {
+    ignores: ["dist/**", "node_modules/**", "build/**"],
+  },
   js.configs.recommended,
   {
     files: ["src/**/*.{ts,tsx}"],
